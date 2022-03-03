8028e55811c3543f4359e4544914224f1f5e6cc4
Grace Wang <gracewang92@gmail.com>
2022-03-03 12:28:50 +0100

    adjust deploy script slightly

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ERC721NFT = await ethers.getContractFactory("ERC721NFT");
+  const nft = await ERC721NFT.deploy("Snaps", "SNAPS");
 
-  await greeter.deployed();
+  await nft.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("NFT collection deployed to:", nft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
